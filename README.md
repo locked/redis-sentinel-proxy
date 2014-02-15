2e73ee03baeadb0b86d622035d43727c5c56b805
Adam Etienne @ n210 <eadam@lunasys.fr>
2014-02-15 22:32:38 +0100

    auto make

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
  This is a PROTOTYPE
 
+== compile ==
+
+ - copy erts-5.9.1 to ./
+ - copy kernel, stdlib, sasl to lib/
+ - run release.sh &lt;dest_path&gt;
+
 == links ==
 
  - http://www.erlang.org/doc/system_principles/create_target.html
--- release.sh
+++ release.sh
@@ -3,6 +3,10 @@
 INSTALL_PATH=$1
 INSTALL_CMD="target_system:install(\"redis\", \"$INSTALL_PATH\"), init:stop()."
 
+cd lib/redis-1.0/
+erl -eval 'make:all(), init:stop().'
+cd ../../
+
 erl -pa lib/redis-1.0/ebin/ -eval 'target_system:create("redis"), init:stop().'
 #echo $INSTALL_CMD
 erl -pa lib/redis-1.0/ebin/ -eval "$INSTALL_CMD"
